f91886d8dddb790c69ae1d34675543321b9755a5
Victor Ferraresi <victor_ferraresi@hotmail.com>
2018-02-24 20:08:13 -0300

    :sparkles: Added the terrain and the player object to the scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 5.0f;
+	public float jumpHeight = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+	}
+}
