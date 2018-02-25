ac8b8321077320a60d2bd843a1b4ceaf72c27aa4
Victor Ferraresi <victor_ferraresi@hotmail.com>
2018-02-24 22:18:18 -0300

    :sparkles: Added leg movement for the dinosaur

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	private float moveSpeed = 3.0f;
+	private float moveSpeed = 6.0f;
 	private float jumpHeight = 10.0f;
+
+	private int actualSprite = 0;
+	private float spriteInterval = 0.1f;
+	private Sprite[] sprites = new Sprite[2];
 
 	// Use this for initialization
 	void Start () {
-		
+		sprites = Resources.LoadAll<Sprite> ("Art/Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		spriteInterval -= Time.deltaTime;
+		if ( spriteInterval < 0 )
+		{
+			spriteInterval = 0.1f;
+			GetComponent<SpriteRenderer> ().sprite = sprites[actualSprite];
+			actualSprite = 1 - actualSprite;
+		}
+
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
 		if(Input.GetKeyDown(KeyCode.Space)) {
