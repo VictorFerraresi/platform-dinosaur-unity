108930ba05f429f8da8c9621ca7771f707ea3dcb
Victor Ferraresi <victor_ferraresi@hotmail.com>
2018-04-08 19:28:23 -0300

    :sparkles: Added a Score counter to track how far the dino is going

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+
+	public Text scoreText;
+	private int score;
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score++;
+		UpdateScore ();
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+}
